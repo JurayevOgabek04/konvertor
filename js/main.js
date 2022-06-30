ddb858bfbce8b5773d1477523aecf10cb561be8c
JurayevOgabek04 <jurayevogabek0304>
2022-06-30 11:47:05 +0500

    script

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// const elBudgerExpanse = document.querySelector(".budget_expense")
+// const elBudgerExpanse = document.querySelector(".divItem")
 const elForm = document.querySelector(".expanse")
 const elFormBudger = document.querySelector(".budget")
 const elList = document.querySelector(".list-item")
@@ -15,20 +15,31 @@ const elBalanse = document.querySelector(".balanse")
 // LIST
 
 let todos = []
-
 let budgerlist = []
 let expanselist = []
 
+elList.addEventListener("click", evt => {
+    if (evt.target.matches(".closeBtn")) {
+        const todos = JSON.parse(window.localStorage.getItem("todos"))
+        const closeId = Number(evt.target.dataset.closeId)
+        const foundId = todos.findIndex(index => index.id === closeId)
+        todos.splice(foundId, 1)
+        elList.innerHTML = null
+        window.localStorage.setItem("todos", JSON.stringify(todos))
+        renderTodo(todos, elList)
+
+    }
+
+})
+
 
 const renderTodo = function (arr, node) {
-
-
     arr.forEach(todo => {
 
-        const newlList = document.createElement("ul")
-        const newTitle = document.createElement("li")
-        const newValue = document.createElement("li")
-        const newTime = document.createElement("li")
+        const newlList = document.createElement("li")
+        const newTitle = document.createElement("h3")
+        const newValue = document.createElement("p")
+        const newTime = document.createElement("span")
         const newClodeBtn = document.createElement("button")
 
         //  SET ATTRIBUTE
@@ -37,19 +48,17 @@ const renderTodo = function (arr, node) {
         newTitle.setAttribute("class", "d-flex justify-content-between col-4 h3")
         newValue.setAttribute("class", "d-flex justify-content-between col-4 h3")
         newTime.setAttribute("class", "d-flex justify-content-between col-4 h3")
-        newClodeBtn.setAttribute("class", "btn btn-outline-danger")
+        newClodeBtn.setAttribute("class", "btn btn-outline-danger closeBtn")
 
         // TEXT CONTENT
-
         newTitle.textContent = todo.title
         newValue.textContent = todo.value
-        newTime.textContent = `${
-            setInterval(function() {
-                return (new Date()).getMinutes() - todo.time
-            }, 1000)
-        } - agoo`
+        newTime.textContent = todo.time
         newClodeBtn.textContent = "close"
 
+
+
+        newClodeBtn.dataset.closeId = todo.id
 
 
 
@@ -61,24 +70,6 @@ const renderTodo = function (arr, node) {
     })
 }
 
-// TEXT CONTENT FUNCTION
-
-const Valuefunc = function () {
-    if (budgerlist.length == 0) {
-        elBudgerScore.textContent = 0,
-        elExpenseScore.textContent = expanselist.reduce((a, b) => a + b),
-        elBalanse.textContent =  - expanselist.reduce((a, b) => a + b)
-    } else if(expanselist.length == 0){
-        elBudgerScore.textContent = budgerlist.reduce((a, b) => a + b),
-        elExpenseScore.textContent = 0,
-        elBalanse.textContent = budgerlist.reduce((a, b) => a + b)
-    } else {
-        elBudgerScore.textContent = budgerlist.reduce((a, b) => a + b),
-        elExpenseScore.textContent = expanselist.reduce((a, b) => a + b),
-        elBalanse.textContent = budgerlist.reduce((a, b) => a + b) - expanselist.reduce((a, b) => a + b)
-    }
-
-}
 
 elFormBudger.addEventListener("click", evt => {
     evt.preventDefault()
@@ -88,6 +79,8 @@ elFormBudger.addEventListener("click", evt => {
 
     // PUSH LIST
     budgerlist.push(Number(budgerInpValue))
+
+    window.localStorage.setItem("budgerlist", JSON.stringify(budgerlist))
 
     // TEXT CONTENT FUNCTION
     Valuefunc()
@@ -100,27 +93,27 @@ elFormBudger.addEventListener("click", evt => {
 })
 
 
-
-
 elForm.addEventListener("submit", evt => {
     evt.preventDefault()
 
     const expenseInpValue = Number(elExpenseInp.value)
-    // console.log(expenseInpValue);
     const expenseNameValue = elExpenseName.value
 
     const todo = {
         id: todos[todos.length - 1]?.id + 1 || 0,
         title: expenseNameValue,
         value: expenseInpValue,
-        // time: `${(new Date()).getDate()} / ${(new Date()).getMonth() + 1} / ${(new Date()).getFullYear()}`
-        time: (new Date()).getMinutes()
+        time: `${(new Date()).getDay()} / ${(new Date()).getMonth() + 1} / ${(new Date()).getFullYear()}`
+
+
     }
 
     // LIST PUSH
     expanselist.push(Number(todo.value))
     todos.push(todo)
 
+    window.localStorage.setItem("todos", JSON.stringify(todos))
+    window.localStorage.setItem("expanselist", JSON.stringify(expanselist))
 
     // TEXT CONTENT FUNCTION
     Valuefunc()
@@ -134,3 +127,23 @@ elForm.addEventListener("submit", evt => {
 
     renderTodo(todos, elList)
 })
+
+
+// TEXT CONTENT FUNCTION
+
+const Valuefunc = function () {
+
+    const expanselist = JSON.parse(window.localStorage.getItem("expanselist")) 
+    const budgerlist = JSON.parse(window.localStorage.getItem("budgerlist"))
+
+    const budgerSum = budgerlist?.reduce((a, b) => a + b) || 0
+    const expenseSum = expanselist?.reduce((a, b) => a + b) || 0
+
+
+    elBudgerScore.textContent = budgerSum
+    elExpenseScore.textContent = expenseSum
+    elBalanse.textContent = budgerSum - expenseSum
+
+}
+
+Valuefunc()
